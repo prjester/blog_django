c5e0c0570e2c155fadb3dbca1ef0f70380e2e26b
Alexander <mr.jester-os@yandex.ru>
2024-02-10 15:57:21 +0300

    panigator

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list( request ):
-    posts = Post.published.all()
-    return render( request, 'blog/post/list.html', { 'posts': posts } )
+    object_list = Post.published.all()
+    panigator = Paginator( object_list, 3 )
+    page = request.GET.get('page')
+    try:
+        posts = panigator.page(page)
+    except PageNotAnInteger:
+        posts = panigator.page(1)
+    except EmptyPage:
+        posts = panigator.page(panigator.num_pages)
+
+    return render( request, 'blog/post/list.html', {'page': page, 'posts': posts } )
 
 def post_detail( request, year, month, day, post ): 
     post = get_object_or_404( Post, slug = post, status = 'published', publish__year = year, publish__month = month, publish__day = day )
